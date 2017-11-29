0ed55d4ab0a3269c75f6364143e936c880ffdf8a
GalZeevi <33779694+GalZeevi@users.noreply.github.com>
2017-11-29 15:52:05 +0200

    Add files via upload

---
+++ app/src/main/java/com/googleworkshop/taxipool/AutocompleteActivity.java
@@ -0,0 +1,64 @@
+package com.example.galzeevi.google_workshop_preferences;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+
+/**
+ * Created by Gal Ze'evi on 11/28/2017.
+ */
+
+public class AutocompleteActivity extends AppCompatActivity {
+    private static final String TAG = PreferencesActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE = "com.example.galzeevi.google_workshop_preferences.MESSAGE";
+    public static Place place1 = null;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.autocomplete_layout);
+
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+    /*
+    * The following code example shows setting an AutocompleteFilter on a PlaceAutocompleteFragment to
+    * set a filter returning only results with a precise address.
+    */
+        AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
+                .setTypeFilter(AutocompleteFilter.TYPE_FILTER_ADDRESS)
+                .build();
+        autocompleteFragment.setFilter(typeFilter);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName());//get place details here
+                place1 = place;
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
+    }
+
+    public void writeLocation(View view){
+        if(place1 != null) {
+            Intent intent = new Intent(this, PreferencesActivity.class);
+            intent.putExtra(EXTRA_MESSAGE, place1.getName().toString());
+            startActivity(intent);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/googleworkshop/taxipool/PreferencesActivity.java
@@ -0,0 +1,85 @@
+package com.example.galzeevi.google_workshop_preferences;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.google.android.gms.location.places.Place;
+
+public class PreferencesActivity extends AppCompatActivity {
+    public static int buttonSearch = 0;
+    public static String dest = "Destination";
+    public static Place destPlace = null;
+    public static String origin = "Your current location";
+    public static Place originPlace = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+
+        Spinner timeSpinner = (Spinner) findViewById(R.id.time);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> tAdapter = ArrayAdapter.createFromResource(this,
+                R.array.hours, R.layout.spinner_item);
+        // Specify the layout to use when the list of choices appears
+        tAdapter.setDropDownViewResource(R.layout.spinner_item);
+        // Apply the adapter to the spinner
+        timeSpinner.setAdapter(tAdapter);
+
+        Spinner genderSpinner = (Spinner) findViewById(R.id.gender);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> gAdapter = ArrayAdapter.createFromResource(this,
+                R.array.gender, R.layout.spinner_item);
+        // Specify the layout to use when the list of choices appears
+        gAdapter.setDropDownViewResource(R.layout.spinner_item);
+        // Apply the adapter to the spinner
+        genderSpinner.setAdapter(gAdapter);
+
+        if(AutocompleteActivity.place1 != null) {
+            // Get the Intent that started this activity and extract the string
+            Intent intent = getIntent();
+            String message = intent.getStringExtra(AutocompleteActivity.EXTRA_MESSAGE);
+            Button button;
+            // Capture the layout's TextView and set the string as its text
+            if(buttonSearch == 1){
+                button = findViewById(R.id.destination);
+                destPlace = AutocompleteActivity.place1;
+                dest = message;
+                Button originButton = findViewById(R.id.origin);
+                originButton.setText(origin);
+            }
+            else{
+                button = findViewById(R.id.origin);
+                originPlace = AutocompleteActivity.place1;
+                origin = message;
+                Button destButton = findViewById(R.id.destination);
+                destButton.setText(dest);
+            }
+            button.setText(message);
+        }
+        else{//delete?
+            Button destButton = findViewById(R.id.destination);
+            destButton.setText(dest);
+            Button originButton = findViewById(R.id.origin);
+            originButton.setText(origin);
+        }
+
+    }
+
+    public void searchDestLocation(View view){
+        Intent intent = new Intent(this, AutocompleteActivity.class);
+        buttonSearch = 1;
+        startActivity(intent);
+    }
+    public void searchOriginLocation(View view){
+        Intent intent = new Intent(this, AutocompleteActivity.class);
+        buttonSearch = 0;
+        startActivity(intent);
+    }
+
+}
