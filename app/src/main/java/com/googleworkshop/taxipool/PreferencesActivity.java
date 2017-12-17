e77a068a0e2141a0b28968bc8bcd5e85c63f45e6
Jerafi <dandushj@gmail.com>
2017-12-17 22:47:52 +0200

    Added the "Save as home" button and supported the operation

--- app/src/main/java/com/googleworkshop/taxipool/PreferencesActivity.java
+++ app/src/main/java/com/googleworkshop/taxipool/PreferencesActivity.java
@@ -2,20 +2,29 @@ package com.googleworkshop.taxipool;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.Spinner;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBufferResponse;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class PreferencesActivity extends AppCompatActivity {
     public static int buttonSearch = 0;
@@ -25,6 +34,18 @@ public class PreferencesActivity extends AppCompatActivity {
     public static Place originPlace = null;
     protected FusedLocationProviderClient mFusedLocationClient;
     protected static Location currLocation;
+    protected SharedPreferences homeSettings;
+    protected SharedPreferences.Editor homePrefEditor;
+    protected CheckBox homeCBox;
+    protected GeoDataClient mGeoDataClient;
+    protected Button destButton;
+    protected Button originButton;
+    public final String HOME_ID = "HOME_ID";
+    public final String HOME_SAVED = "HOME_SAVED";
+    public final String homePrefs = "UserHomePreferences";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +53,22 @@ public class PreferencesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_preferences);
         //XXX JERAFI ADDED ME FOR LOCATION
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mGeoDataClient = Places.getGeoDataClient(this, null);
+        homeCBox =  findViewById(R.id.HomeCheckBox);
+        homeSettings = getSharedPreferences(homePrefs, 0);
+        homePrefEditor = homeSettings.edit();
+        destButton = findViewById(R.id.destination);
+        originButton = findViewById(R.id.origin);
+
+
+        homeCBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                homePrefEditor.putBoolean(HOME_SAVED,compoundButton.isChecked());
+                homePrefEditor.apply();
+            }
+        });
+        initializeHome();
 
         Spinner timeSpinner = (Spinner) findViewById(R.id.time);
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -58,28 +95,52 @@ public class PreferencesActivity extends AppCompatActivity {
             Button button;
             // Capture the layout's TextView and set the string as its text
             if(buttonSearch == 1){
-                button = findViewById(R.id.destination);
+                button = destButton;
                 destPlace = AutocompleteActivity.place1;
                 dest = message;
-                Button originButton = findViewById(R.id.origin);
                 originButton.setText(origin);
             }
             else{
-                button = findViewById(R.id.origin);
+                button = originButton;
                 originPlace = AutocompleteActivity.place1;
                 origin = message;
-                Button destButton = findViewById(R.id.destination);
-                destButton.setText(dest);
+//                destButton.setText(dest);
             }
             button.setText(message);
         }
         else{//delete?
-            Button destButton = findViewById(R.id.destination);
             destButton.setText(dest);
-            Button originButton = findViewById(R.id.origin);
             originButton.setText(origin);
         }
+        if (destPlace==null){
+            homeCBox.setClickable(false);
+        }
+        else{
+            homeCBox.setClickable(true);
+            homeCBox.setChecked(homeSettings.getBoolean(HOME_SAVED,false));
+            destButton.setText(destPlace.getName());
+        }
 
+    }
+
+    private void initializeHome(){
+        //JERAFI Using default home location if saved
+        if ((destPlace == null) && (homeSettings.getBoolean(HOME_SAVED,false))){
+            Task<PlaceBufferResponse> response = mGeoDataClient.getPlaceById(homeSettings.getString(HOME_ID,null));
+            response.addOnCompleteListener(new OnCompleteListener<PlaceBufferResponse>() {
+                @Override
+                public void onComplete(@NonNull Task<PlaceBufferResponse> task) {
+                    if (task.isSuccessful()){
+                        PlaceBufferResponse homeBuffer = task.getResult();
+                        destPlace = homeBuffer.get(0);
+                        homeCBox.setChecked(true);
+                        homeCBox.setClickable(true);
+                        destButton.setText(destPlace.getName());
+
+                    }
+                }
+            });
+        }
     }
 
     public void searchDestLocation(View view){
@@ -92,11 +153,12 @@ public class PreferencesActivity extends AppCompatActivity {
         buttonSearch = 0;
         startActivity(intent);
     }
-    //XXX JERAFI ADDED TO GO TO THE MAP
 
     public void goToRoute(View view){
         Intent intent = new Intent(this, MatchScreenActivity.class);
-
+        if (destPlace == null){
+            return;
+        }
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -117,7 +179,14 @@ public class PreferencesActivity extends AppCompatActivity {
                 }
             }
         });
+        if (homeCBox.isChecked()) {
+            homePrefEditor.putString(HOME_ID, destPlace.getId());
+            homePrefEditor.putBoolean(HOME_SAVED,true);
+        }
+        else{
+            homePrefEditor.putBoolean(HOME_SAVED,false);
+        }
+        homePrefEditor.apply();
         startActivity(intent);
     }
-
 }
