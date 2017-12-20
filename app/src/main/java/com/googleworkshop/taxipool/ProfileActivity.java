413bf1c8e3ec53ce60acca607918b8704821789e
Benjamin Mizrahi <benjajamin@gmail.com>
2017-12-20 18:59:20 +0200

    Added firebase user integration to ProfileActivity

--- app/src/main/java/com/googleworkshop/taxipool/ProfileActivity.java
+++ app/src/main/java/com/googleworkshop/taxipool/ProfileActivity.java
@@ -1,8 +1,30 @@
 package com.googleworkshop.taxipool;
 
-/**
- * Created by Ilana on 17/12/17.
- */
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ProfileActivity {
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        mAuth=FirebaseAuth.getInstance();
+        FirebaseUser user=mAuth.getCurrentUser(); //get current user
+
+        TextView userName=(TextView)findViewById(R.id.user_name);
+        ImageView imageView = (ImageView)findViewById(R.id.profile_img);
+
+        userName.setText(user.getDisplayName()); //Set user name
+        Glide.with(getApplicationContext()).load(user.getPhotoUrl()).into(imageView); //Set profile image
+    }
+
 }
